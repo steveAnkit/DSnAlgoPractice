aafd5e2597fce7bb3c9ef213050af8089abb38da
steveAnkit <ankitsharma1702@gmail.com>
2016-06-18 23:16:15 +0530

    Kth non-epeating element

---
+++ src/com/geeks/practice/kthNonRepeatingElement/KthNonRepeatingElement.java
@@ -0,0 +1,38 @@
+package com.geeks.practice.kthNonRepeatingElement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KthNonRepeatingElement {
+
+	public static void main(String[] args) {
+        String inputString = "aassddeffguui";
+        int position = 3;
+        getNonRepeatingElement(inputString, position);
+		
+	}
+	
+	
+	public static String getNonRepeatingElement(String set, int position){
+		Map<Character, Integer> map = new HashMap<>();
+		for (Character ch : set.toCharArray()) {
+			if(map.get(ch) == null)
+				map.put(ch, 1);
+			else{
+				map.put(ch, map.get(ch)+1);
+			}
+		}
+		int count =0;
+		for (Character ch : set.toCharArray()) {
+			if(map.get(ch) == 1){
+				count++;
+				if(count == position)
+				System.out.println(ch);
+			}
+				
+		}
+		
+		return null;
+	}
+
+}
