ad030322c43593ce7eda37e4c7366e6795f52f4f
steveAnkit <ankitsharma1702@gmail.com>
2016-06-19 02:24:11 +0530

    a to the power b with recursion

---
+++ src/com/geeks/practice/aToPowerB/AToPowerB.java
@@ -0,0 +1,27 @@
+package com.geeks.practice.aToPowerB;
+
+import java.util.Scanner;
+
+public class AToPowerB {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int num1 = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		System.out.println(printPower(num1, num2, 1));
+		
+	}
+	
+	public static int printPower(int a, int b, int count){
+
+		if(count == b)
+			return a;
+		else{
+			++count;
+			int sum = a *  printPower(a, b, count);
+			return sum;
+		}
+	}
+
+}
