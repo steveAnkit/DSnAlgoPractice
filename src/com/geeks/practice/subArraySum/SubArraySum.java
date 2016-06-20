aa6b3b0e36faa43682c8e15ccc1f1bca46ce8952
steveAnkit <ankitsharma1702@gmail.com>
2016-06-20 23:32:04 +0530

    Sub array equals to given sum

---
+++ src/com/geeks/practice/subArraySum/SubArraySum.java
@@ -0,0 +1,36 @@
+package com.geeks.practice.subArraySum;
+
+public class SubArraySum {
+
+	public static void main(String[] args) {
+      int[] array = {2,3,5,1,};
+      int sum = 6;
+      int size = array.length;
+      getSubArray(array, sum, size);
+      
+	}
+	
+	public static void getSubArray(int[] array , int sum, int n){
+		
+		int currentSum = array[0];
+		int start = 0;
+		for (int i = 1; i <= array.length; i++) {
+			
+			 while(currentSum > sum){
+				 currentSum = currentSum - array[start];
+				 start++;
+			 }
+			
+			if(currentSum == sum){
+				System.out.println("MATCH FOUND BETWEEN  "+ start +" AND "+ (i - 1) );
+				break;
+			}
+            
+			if(i < n)
+			currentSum = currentSum + array[i]; 
+			
+		}
+		
+	}
+
+}
